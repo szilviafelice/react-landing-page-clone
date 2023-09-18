09eb24b917b5d6c7b821bd1f06e68bb4fc2741da
szilviafelice <szilvia.felice@gmail.com>
2023-09-18 17:13:31 -0400

    Style of the header added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import './App.css';
-import logo from './logo.svg';
+import styles from './App.module.scss';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/#">Logo - by thoughtbot</a>
+            </li>
+            <li>
+              <a href="/#">Trails</a>
+            </li>
+            <li>
+              <a href="/#">Videos</a>
+            </li>
+            <li>
+              <a href="/#">Flashcards</a>
+            </li>
+            <li>
+              <a href="/#">Forum</a>
+            </li>
+            <li>
+              <a href="/#">Search</a>
+            </li>
+            <li>
+              <a href="/#">About Us</a>
+            </li>
+            <li>
+              <a href="/#">Sign In</a>
+            </li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        Main
+        <section>Section 1</section>
+        <section>Section 2</section>
+      </main>
+      <footer>Footer</footer>
+    </>
   );
 }
